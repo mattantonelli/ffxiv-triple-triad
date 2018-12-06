d4994dcc1d17bd0ecf18702447cd1fde3fdf510d
Matt Antonelli <contact@raelys.com>
2018-12-06 20:46:43 +0000

    Update cards table
    Improves styling. Adds type images and NPC sources. Removes sell column.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,5 @@
 class CardsController < ApplicationController
   def index
-    @cards = Card.all.includes(:type).reverse
+    @cards = Card.all.includes(:npc_sources).order(id: :desc)
   end
 end
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -7,7 +7,21 @@ module CardsHelper
     image_tag("cards/large/#{card.id}.png")
   end
 
+  def type_image(card)
+    id = card.card_type_id
+    image_tag("cards/types/#{id}.png") if id > 0
+  end
+
   def stars(card)
     (fa_icon('star') * card.stars).html_safe
+  end
+
+  def stat(card, side)
+    value = card[side]
+    value == 10 ? 'A' : value
+  end
+
+  def source(card)
+    card.npc_sources.pluck(:name).join(', ')
   end
 end
