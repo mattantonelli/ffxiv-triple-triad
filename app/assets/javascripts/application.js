0da3ab05f4a69f10e33071d3f61b4ef6aaa09c94
Matt Antonelli <contact@raelys.com>
2018-12-19 23:51:02 +0000

    Remove unused assets, etc.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require jquery3
 //= require popper
--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,9 @@ require_relative 'boot'
 
 require "rails"
 require "active_model/railtie"
-require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 require "action_controller/railtie"
 require "action_view/railtie"
-require "action_cable/engine"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
