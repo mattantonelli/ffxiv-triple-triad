4e847ab067e0aeb8cc6f852f131522261e79ddc2
Matt Antonelli <contact@raelys.com>
2018-12-19 02:01:02 +0000

    Clean up logs
    Removes request ID and the duplicated lograge timestamp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  # config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -87,7 +87,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.lograge.enabled = true
-  config.lograge.custom_options = lambda do |event|
-    { time: Time.now }
-  end
 end
