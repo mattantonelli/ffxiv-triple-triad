d3135a9a5befee19f78eb8c0d0b7ad7c9947ae90
Matt Antonelli <contact@raelys.com>
2018-12-28 04:09:17 +0000

    Add not found response for User API endpoint

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,15 +2,17 @@ class Api::UsersController < ApiController
   def show
     @user = User.find_by(uid: params[:id])
 
-    if @user.public_cards
+    if !@user.present?
+      render json: { status: 404, error: 'Not found' }, status: :not_found
+    elsif !@user.public_cards
+      render json: { status: 403, error: "User's card collection is set to private" }, status: :forbidden
+    else
       @total = Card.count
       @user_cards = @user.cards
 
       limit = params[:limit_missing].present? ? params[:limit_missing].to_i : 5
       random_ids = ((1..@total).to_a - @user_cards).sample(limit)
       @random_cards = Card.includes(:type, :npc_sources, :sources, :pack).where(id: random_ids).order(:patch, :id)
-    else
-      render json: { status: 403, error: "User's card collection is set to private" }, status: :forbidden
     end
   end
 end
