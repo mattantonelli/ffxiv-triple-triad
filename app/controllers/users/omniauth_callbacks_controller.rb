7c09056ac4e1346a70f4c42ebf436615e5854808
Matt Antonelli <contact@raelys.com>
2018-12-30 18:47:36 +0000

    Add failure handling to OAuth callback controller
    If the user chooses to Cancel the authentication, this will result in a
    failure, which should send them back to the home page.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,4 +4,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sign_in(@user)
     redirect_to my_cards_path
   end
+
+  def failure
+    redirect_to root_path
+  end
 end
