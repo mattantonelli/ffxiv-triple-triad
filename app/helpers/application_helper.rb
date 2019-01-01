a990cdc3a3ce32c714a507ce7bc8bca12593755b
Matt Antonelli <contact@raelys.com>
2019-01-01 03:30:48 +0000

    Handle missing avatars
    Identifies users who do not have Discord avatars and skips rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,9 @@ module ApplicationHelper
   def format_date(date)
     date.in_time_zone('America/New_York').strftime('%e %b %Y %H:%M')
   end
+
+  def avatar(user)
+    # Discord users without avatars will end in a /
+    image_tag(user.avatar_url, class: 'avatar') unless user.avatar_url&.match?(/\/\Z/)
+  end
 end
