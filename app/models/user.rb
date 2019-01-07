890faa3be57100bf8504a5d4b5a4f1e2662579e2
Matt Antonelli <contact@raelys.com>
2019-01-07 14:13:56 +0000

    Sanitize usernames
    Fixes a bug where users with special characters in their username could
    not create an account by sanitizing the username. Also keeping the
    username up to date with the rest of the info.

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,15 +40,19 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
+    # Clean up any special characters in the username
+    username = auth.info.name.encode(Encoding.find('ASCII'), { invalid: :replace, undef: :replace, replace: '' })
+
     discord_user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.username = auth.info.name
+      user.username = username
       user.discriminator = auth.extra.raw_info.discriminator
       user.avatar_url = auth.info.image
     end
 
     # Keep existing Discord users' data up to date
     if discord_user.persisted?
-      discord_user.update(discriminator: auth.extra.raw_info.discriminator,
+      discord_user.update(username: username,
+                          discriminator: auth.extra.raw_info.discriminator,
                           avatar_url: auth.info.image)
     end
 
