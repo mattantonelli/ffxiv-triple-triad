8f2981c99d7ac5a642331e1fe63fcd1a497e42ad
Matt Antonelli <contact@raelys.com>
2019-04-14 16:05:02 +0000

    Adapt play guide link to user's locale

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+  def play_guide_link
+    region = case(I18n.locale)
+             when :fr then 'fr'
+             when :de then 'de'
+             when :ja then 'jp'
+             else 'na'
+             end
+
+    link_to("Play Guide #{fa_icon('external-link')}".html_safe,
+            "https://#{region}.finalfantasyxiv.com/lodestone/playguide/contentsguide/goldsaucer/tripletriad",
+            target: '_blank', class: 'nav-link')
+  end
 end
