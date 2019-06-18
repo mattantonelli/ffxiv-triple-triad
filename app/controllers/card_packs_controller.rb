fab10388c3a6636148e896703e938eaf5b1da150
Matt Antonelli <contact@raelys.com>
2019-06-18 12:40:36 +0000

    Restore transparency for owned cards on Packs page
    Forgot to update this to pluck the IDs of the owned cards after
    transitioning to the new database. Resolves #5

--- app/controllers/card_packs_controller.rb
+++ app/controllers/card_packs_controller.rb
@@ -1,6 +1,6 @@
 class CardPacksController < ApplicationController
   def index
     @packs = Pack.all.includes(:cards)
-    @owned_cards = current_user.cards if user_signed_in?
+    @owned_cards = current_user.cards.pluck(:id) if user_signed_in?
   end
 end
