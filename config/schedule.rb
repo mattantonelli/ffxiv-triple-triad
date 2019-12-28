167e5496a6d0c086d2d06108abb5903f3d904bfc
Matt Antonelli <contact@raelys.com>
2019-12-28 15:36:22 +0000

    Update ownership task schedule
    Due to high load and a low priority on the data, this task will now run
    nightly instead of hourly.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # Explicitly point to the rbenv bundle shim so the cronjob can find it
 set :bundle_command, '~/.rbenv/shims/bundle exec'
 
-every '0 * * * *' do
+every '0 3 * * *' do
   rake 'ownership:cache'
 end
