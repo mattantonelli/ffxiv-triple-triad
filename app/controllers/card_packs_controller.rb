21cc4c7dea0a8852900b6718fb0923f4744b8a4b
Matt Antonelli <contact@raelys.com>
2020-03-26 23:30:20 +0000

    Fix N+1 query on card types for Packs

--- app/controllers/card_packs_controller.rb
+++ app/controllers/card_packs_controller.rb
@@ -1,6 +1,6 @@
 class CardPacksController < ApplicationController
   def index
-    @packs = Pack.all.includes(:cards)
+    @packs = Pack.all.includes(cards: :type)
     @owned_cards = current_user.cards.pluck(:id) if user_signed_in?
   end
 end
