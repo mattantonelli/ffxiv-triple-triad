569df3179a349ec18cd2b2f787fdae7d115f5338
Matt Antonelli <contact@raelys.com>
2021-04-07 12:36:20 +0000

    Update deck validation for Patch 5.5

--- app/models/concerns/deck_validator.rb
+++ app/models/concerns/deck_validator.rb
@@ -4,8 +4,15 @@ class DeckValidator < ActiveModel::Validator
       deck.errors.add(:cards, :invalid_size)
     end
 
-    if deck.cards.map(&:stars).count { |stars| stars >= 4 } > 1
-      puts deck.cards.map(&:id).join(' ')
+    stars = deck.cards.map(&:stars).freeze
+
+    # Cards of ★★★★★ rarity: 1 card per deck
+    if stars.count { |star| star == 5 } > 1
+      deck.errors.add(:cards, :too_many_fives)
+    end
+
+    # Cards of ★★★★ rarity or more: 2 cards per deck
+    if stars.count { |star| star >= 4 } > 2
       deck.errors.add(:cards, :too_many_rares)
     end
 
