1f081c33d30c325f850748f21c14a907ac31e08d
Matt Antonelli <contact@raelys.com>
2022-07-20 19:45:59 +0000

    Fix irreversible migration

--- db/migrate/20220720183925_add_decimals_to_npc_coordinates.rb
+++ db/migrate/20220720183925_add_decimals_to_npc_coordinates.rb
@@ -1,6 +1,11 @@
 class AddDecimalsToNPCCoordinates < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :npcs, :x, :decimal, precision: 3, scale: 1
     change_column :npcs, :y, :decimal, precision: 3, scale: 1
+  end
+
+  def down
+    change_column :npcs, :x, :integer
+    change_column :npcs, :y, :integer
   end
 end
